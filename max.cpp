53d26737fb9ba1ac7689f7d2bfe3f92e70d8a2b2
khanmosajjid <mosajjid.khan@gmail.com>
2019-07-30 15:28:54 +0530

    print the number in ascending order whose sum is equal to target

--- max.cpp
+++ max.cpp
@@ -1,11 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int max(long long int a[],int n){
+long long int max(int n){
     long long int i,max=0;
     for(i=0;i<n;i++){
-          if(a[i]>max){
-              max=a[i];
+      cin>>a
+          if(a>max){
+              max=a;
           }
     }
     return max;
@@ -13,11 +14,9 @@ int max(long long int a[],int n){
 }
 int main() {
     int n,i;
-    long long int a[100000];
+    
     cin>>n;
-    for(i=0;i<n;i++){
-        cin>>a[i];
-    }
-    cout<<max(a,n);
+    
+    cout<<max(n);
 	return 0;
 }
