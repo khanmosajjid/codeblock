de3a9a0b357c59ea461888dd3562c81f83761d41
khanmosajjid <mosajjid.khan@gmail.com>
2019-08-25 21:40:34 +0530

    finding power of a using recursion

---
+++ pwerFun.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int pow(int a,int b){  //find the power of a power b using recursion
+	if(b==1){
+		return a;   //base condition
+	}
+
+	return a*pow(a,b-1); //recusive function
+}
+
+int main(){
+	int a,b;
+	cin>>a>>b;
+	cout<<pow(a,b)<<endl;
+	return 0;
+}
